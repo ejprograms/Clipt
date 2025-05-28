638ba4366b8a58c712a8f9a0a7bd79b611220dc9
ejprograms <elyseprogram@gmail.com>
2025-05-28 20:59:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+export default defineConfig({
+  base: "Clipt",
+})
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
